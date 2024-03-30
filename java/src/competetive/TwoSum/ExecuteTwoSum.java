f4dde5ee358e054f54b8adc2f9145fe9ed243127
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-03-30 21:54:28 +0530

    TwoSum with a better folder structure

---
+++ java/src/competetive/TwoSum/ExecuteTwoSum.java
@@ -0,0 +1,9 @@
+package competetive.TwoSum;
+import java.util.Arrays;
+
+class ExecuteTwoSum {
+  public static void main(String[] args){
+    TwoSum twoSum = new TwoSum(14, new int[]{ 1,3,6,4,18,9,21,13,14,19 });
+    System.out.println(Arrays.toString(twoSum.indices()));
+  }
+}
---
+++ java/src/competetive/TwoSum/TwoSum.java
@@ -0,0 +1,29 @@
+package competetive.TwoSum;
+import java.util.HashMap;
+
+class TwoSum {
+  private int target;
+  HashMap<Integer, Integer> list = new HashMap<Integer, Integer>();
+  private int[] indices = { 0,0 };
+
+  TwoSum(int theTarget, int[] rawList){
+    target = theTarget;
+    for(int i=0; i < rawList.length; i++) {
+      list.put(rawList[i], i);
+    }
+    findIndices(target, list);
+  }
+
+  public int[] indices(){
+    return indices;
+  }
+
+  public void findIndices(int target, HashMap<Integer, Integer> theList) {
+    for(Integer firstItem: theList.keySet()) {
+      if(theList.containsKey(target - firstItem)) {
+        indices[0] = theList.get(target-firstItem);
+        indices[1] = theList.get(firstItem);
+      }
+    }
+  }
+}
