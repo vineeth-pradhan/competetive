7d68d0f83b3ff25df8fbcbabceda6c29eea9b2e4
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-04 19:08:22 +0530

    Rotate Image solution

--- RotateImage/src/main/java/competetive/Solution.java
+++ RotateImage/src/main/java/competetive/Solution.java
@@ -2,33 +2,27 @@ package competetive;
 import java.util.Arrays;
 
 class Solution {
-    public void rotate(int[][] matrix) {
-        int[][] newMatrix = new int[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                int index = Math.abs(i-(matrix[i].length-1));
-                newMatrix[j][index] = matrix[i][j];
-            }
-        }
-        System.out.print("[");
-        for(int i = 0; i < newMatrix.length; i++){
-            System.out.print("[");
-            for(int j = 0; j < newMatrix[i].length; j++){
-                System.out.print(newMatrix[i][j]);
-                if(j != newMatrix[i].length-1){
-                    System.out.print(",");
-                }
-            }
-            System.out.print("]");
-            if(i != newMatrix[i].length-1){
-                System.out.print(",");
-            }
-        }
-        System.out.print("]");
+  public void rotate(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = i+1; j < matrix[i].length; j++) {
+          int tempValue = matrix[i][j];
+          matrix[i][j] = matrix[j][i];
+          matrix[j][i] = tempValue;
+      }
     }
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length / 2; j++) {
+        int index = matrix[i].length - 1 - j;
+        int tempValue = matrix[i][j];
+        matrix[i][j] = matrix[i][index];
+        matrix[i][index] = tempValue;
+      }
+    }
+    System.out.println(Arrays.deepToString(matrix));
+  }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        sol.rotate(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
-    }
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    sol.rotate(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
+  }
 }
