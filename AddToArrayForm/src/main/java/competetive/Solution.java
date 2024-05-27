0beb8ee8fa55d435838363d4473cfc49ff393a00
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-27 07:46:57 +0530

    Use LinkedList instead of ArrayList. Insertion operations are more needed in this algo and insertions are faster in LL than AL

--- AddToArrayForm/src/main/java/competetive/Solution.java
+++ AddToArrayForm/src/main/java/competetive/Solution.java
@@ -1,38 +1,31 @@
 // https://leetcode.com/problems/add-to-array-form-of-integer/
 package competetive;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Solution {
-  List result = new ArrayList<Integer>();
-  int[] num;
-  int k;
+  List result = new LinkedList<Integer>();
 
-  public Solution(int[] num, int k){
-    this.num = num;
-    this.k = k;
-  }
-
-  public List<Integer> addToArrayForm() {
-    int i = this.num.length;
-    helper(i, this.k, 0);
+  public List<Integer> addToArrayForm(int[] num, int k) {
+    int i = num.length;
+    helper(i, k, 0, num);
     return result;
   }
 
   public static void main(String[] args) {
-    Solution solution = new Solution(new int[]{9}, 23);
-    List result = solution.addToArrayForm();
+    Solution solution = new Solution();
+    List result = solution.addToArrayForm(new int[]{9,9,9,9}, 23);
     System.out.println(result.toString());
   }
 
-  private void helper(int i, int k, int carry){
+  private void helper(int i, int k, int carry, int[] num){
     if(i > 0 || k > 0 || carry > 0){
       int pseudoNumOfI = i > 0 ? num[i-1] : 0;
       int sum = ((k % 10) + pseudoNumOfI + carry);
       this.result.add(0, sum % 10);
       carry = sum / 10;
       i--;
-      helper(i,k/10,carry);
+      helper(i,k/10,carry,num);
     }
   }
 }
