90e29faa4b45c601dc49eda3054ecc09cfeef3d1
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-30 12:14:15 +0530

    Faster than 100% of java online submissions\!

--- RemoveElement/src/main/java/competetive/Solution.java
+++ RemoveElement/src/main/java/competetive/Solution.java
@@ -1,23 +1,14 @@
 // https://leetcode.com/problems/remove-element/
 package competetive;
-import java.util.List;
-import java.util.LinkedList;
 import java.util.Arrays;
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        LinkedList<Integer> queue = new LinkedList<Integer>();
         int k=0;
         for(int i=0; i<nums.length; i++){
             if(nums[i] != val){
-                k++;
-                if(queue.size() != 0){
-                    nums[queue.remove()] = nums[i];
-                    nums[i] = val;
-                    i--;
-                }
+                nums[k++]=nums[i];
             }
-            else if(nums[i] == val){ queue.add(i); }
         }
         System.out.println(Arrays.toString(nums));
         return k;
