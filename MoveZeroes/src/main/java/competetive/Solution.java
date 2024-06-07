f3ec930eca90a8a4e42ab7401a9114766ddd087b
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-06-07 12:35:41 +0530

    Best time complexity(inspired)

--- MoveZeroes/src/main/java/competetive/Solution.java
+++ MoveZeroes/src/main/java/competetive/Solution.java
@@ -7,15 +7,23 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int count = 0;
         for(int i=0; i < nums.length; i++){
-            if(nums[i] == 0){ count+=1; }
-            else{
-                int temp = nums[i-count];
-                nums[i-count] = nums[i];
-                nums[i] = temp;
-            }
+            if(nums[i] != 0){ nums[count++] = nums[i]; }
         }
+        while(count < nums.length){ nums[count++] = 0; }
         System.out.println(Arrays.toString(nums));
     }
+//    public void moveZeroes(int[] nums) {
+//        int count = 0;
+//        for(int i=0; i < nums.length; i++){
+//            if(nums[i] == 0){ count+=1; }
+//            else{
+//                int temp = nums[i-count];
+//                nums[i-count] = nums[i];
+//                nums[i] = temp;
+//            }
+//        }
+//        System.out.println(Arrays.toString(nums));
+//    }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
