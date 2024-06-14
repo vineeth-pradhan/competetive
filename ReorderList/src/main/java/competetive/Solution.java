099be6d18cb858a4a31e4c1831c4536dafd84eec
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-06-14 06:26:53 +0530

    Moving all ListNode definitions to package

--- ReorderList/src/main/java/competetive/Solution.java
+++ ReorderList/src/main/java/competetive/Solution.java
@@ -1,69 +1,14 @@
 // https://leetcode.com/problems/reorder-list/
 package competetive;
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    public ListNode copyNode(){
-        ListNode node = this;
-        ListNode copy = new ListNode();
-        ListNode newHead = copy;
-        while(node != null){
-            copy.val = node.val;
-            node = node.next;
-            if(node != null){ copy.next = new ListNode(); copy = copy.next; }
-        }
-        return newHead;
-    }
-
-    public void constructLinkedList(int[] input, ListNode node, int i){
-        if(i+1 < input.length){
-            ListNode next = new ListNode(input[i+1]);
-            node.next = next;
-            constructLinkedList(input, next, i+1);
-        }
-    }
-
-    public ListNode reverseList(ListNode head){
-        ListNode node = head;
-        ListNode reverse = head;
-        if(node.next == null){}
-        else if(node != null){ reverse = reverseHelper(node, node.next, node); }
-        return reverse;
-    }
-
-    public void printLinkedList(ListNode head){
-        if (head != null) {
-            System.out.print(head.val);
-            if(head.next != null)
-                System.out.print(" -> ");
-            printLinkedList(head.next);
-        }
-        else{ System.out.println(); }
-    }
-
-    private ListNode reverseHelper(ListNode current, ListNode next, ListNode newHead){
-        if(next != null){
-            ListNode temp = next.next;
-            next.next = current;
-            if(current == newHead) { current.next = null; }
-            newHead = reverseHelper(next, temp, newHead);
-        }
-        else{ newHead = current; }
-        return newHead;
-    }
-}
+import com.list.*;
 
 class Solution {
     public void reorderList(ListNode head) {
         ListNode half = head;
         ListNode full = head;
-        while(full != null && full.next != null){
-            full = full.next.next;
-            half = half.next;
+        while(full != null && full.getNext() != null){
+            full = full.getNext().getNext();
+            half = half.getNext();
         }
         ListNode reverse = half.reverseList(half);
         head.printLinkedList(head);
@@ -74,13 +19,13 @@ class Solution {
 
     private ListNode reorderHelper(ListNode a, ListNode b){
         if(a != b){
-            ListNode tempA = a.next;
-            a.next = b;
+            ListNode tempA = a.getNext();
+            a.setNext(b);
             if(b != null){
-                ListNode tempB = b.next;
-                b.next = tempA;
+                ListNode tempB = b.getNext();
+                b.setNext(tempA);
                 if(tempA != tempB){ reorderHelper(tempA, tempB); }
-                else{ tempA.next = null; }
+                else{ tempA.setNext(null); }
             }
         }
         return a;
@@ -92,6 +37,7 @@ class Solution {
 //        int[] inputArray = { 1,7,2,6,3,5,4 };
 //        int[] inputArray = { 1,2,3,4,5,6,7,8 };
 //        int[] inputArray = { 1,8,2,7,3,6,4,5 };
+
         ListNode head = new ListNode(inputArray[0]);
         head.constructLinkedList(inputArray, head, 0);
         solution.reorderList(head);
