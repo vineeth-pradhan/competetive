77c87c6f42d3fc7eb71f4a2344c52a69b17c6da8
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-06-27 11:29:01 +0530

    Alternate approach

--- RotateArray/src/main/java/competetive/Solution.java
+++ RotateArray/src/main/java/competetive/Solution.java
@@ -7,28 +7,29 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int r = k % nums.length;
         int start = nums.length - r;
-        int j = 0;
+        int j = nums.length - 2 * r;
         nums = rotateHelper(nums, k, start, j);
         System.out.println(Arrays.toString(nums));
     }
 
     public int[] rotateHelper(int[] nums, int k, int start, int j){
+        if(k == 0){ return nums; }
         for(int i = start; i < nums.length; i++){
-            if(j < nums.length){
+            if(j < start && j >= 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
                 j++;
             }
         }
-        if(j < nums.length){ return rotateHelper(nums, k, start, j); }
-        else{
-            if(nums.length % k == 1){
-                int i = nums.length - 2;
-                j = nums.length - 1;
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        if(j > k){ return rotateHelper(nums, k, start - k, start - 2 * k); }
+        else {
+            int m = j + k - 1;
+            while(m < k){
+                int temp = nums[m];
+                nums[m] = nums[m+1];
+                nums[m+1] = temp;
+                m++;
             }
             return nums;
         }
@@ -36,8 +37,8 @@ class Solution {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] inputArray = new int[] { 1,2,3,4,5,6,7,8,9,10,11 };
+        int[] inputArray = new int[] { 1,2,3,4,5,6 };
         System.out.println(Arrays.toString(inputArray));
-        sol.rotate(inputArray, 3);
+        sol.rotate(inputArray, 4);
     }
 }
